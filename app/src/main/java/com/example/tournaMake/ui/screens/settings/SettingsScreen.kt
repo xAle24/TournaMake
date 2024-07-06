8ec97e92a3c8303edc393c8940171bfd1ac46f86
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2024-07-06 12:13:39 +0200

    test: changed a test

--- app/src/main/java/com/example/tournaMake/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/tournaMake/ui/screens/settings/SettingsScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,11 +18,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+<<<<<<< Updated upstream
 import androidx.compose.ui.unit.dp
+=======
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+>>>>>>> Stashed changes
 import com.example.tournaMake.data.models.ThemeEnum
 import com.example.tournaMake.data.models.ThemeState
 import com.example.tournaMake.ui.screens.common.BasicScreenWithTheme
 import com.example.tournaMake.ui.theme.LocalBackgroundImageId
+import com.example.tournaMake.ui.theme.MediumPurple
 import com.example.tournaMake.ui.theme.getBackgroundImageId
 
 /**
