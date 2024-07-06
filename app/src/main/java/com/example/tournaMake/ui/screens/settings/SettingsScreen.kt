c4bdfeb7b1188d1f07753f6910e89b28241f9462
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2024-07-06 12:18:29 +0200

    chore: fixed error in stash

--- app/src/main/java/com/example/tournaMake/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/example/tournaMake/ui/screens/settings/SettingsScreen.kt
@@ -1,35 +1,19 @@
 package com.example.tournaMake.ui.screens.settings
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-<<<<<<< Updated upstream
 import androidx.compose.ui.unit.dp
-=======
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
->>>>>>> Stashed changes
 import com.example.tournaMake.data.models.ThemeEnum
 import com.example.tournaMake.data.models.ThemeState
 import com.example.tournaMake.ui.screens.common.BasicScreenWithTheme
-import com.example.tournaMake.ui.theme.LocalBackgroundImageId
-import com.example.tournaMake.ui.theme.MediumPurple
-import com.example.tournaMake.ui.theme.getBackgroundImageId
 
 /**
  * Builds the Settings Screen. Called in SettingsActivity.
