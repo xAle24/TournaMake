580c520dfe08f92aec49d94d6c1ae0434f65b780
Alessio Lizza <alessio.lizza@studio.unibo.it>
2024-07-11 00:15:15 +0200

    added guest profile list

--- app/src/main/java/com/example/tournaMake/activities/ProfileListActivity.kt
+++ app/src/main/java/com/example/tournaMake/activities/ProfileListActivity.kt
@@ -1,15 +1,22 @@
 package com.example.tournaMake.activities
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.example.tournaMake.data.models.ThemeViewModel
-import com.example.tournaMake.ui.screens.menu.MenuScreen
+import com.example.tournaMake.sampledata.AppDatabase
+import com.example.tournaMake.sampledata.GuestProfile
+import com.example.tournaMake.ui.screens.profile.ProfileListScreen
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.get
 import org.koin.androidx.compose.koinViewModel
 
-class MenuActivity : ComponentActivity() {
+class ProfileListActivity : ComponentActivity() {
+    private var appDatabase: AppDatabase? = get<AppDatabase>()
+    private lateinit var guestList: List<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -20,29 +27,23 @@ class MenuActivity : ComponentActivity() {
             // StateFlow objects can't. The 'withLifecycle' part ensures this state
             // is destroyed when we leave this Activity.
             val state = themeViewModel.state.collectAsStateWithLifecycle()
-            MenuScreen(
+            guestList = getGuestProfile()
+            ProfileListScreen(
                 state = state.value,
-                navigateToTournament= this::navigateToTournament,
-                navigateToProfile = this::navigateToListProfile,
-                navigateToSettings = this::navigateToSettings,
-                navigateToFavorites = this::navigateToFavorites
+                guestList
             )
         }
     }
-    private fun navigateToTournament() {
-        val intent = Intent(this, TournamentActivity::class.java)
-        startActivity(intent)
-    }
-    private fun navigateToFavorites() {
-        val intent = Intent(this, FavoritesActivity::class.java)
-        startActivity(intent)
-    }
-    private fun navigateToListProfile() {
-        val intent = Intent(this, ProfileListActivity::class.java)
-        startActivity(intent)
-    }
-    private fun navigateToSettings() {
-        val intent = Intent(this, SettingsActivity::class.java)
-        startActivity(intent)
+
+    private fun getGuestProfile(): List<String> {
+        var guestProfiles: List<GuestProfile> = emptyList()
+        lifecycleScope.launch(Dispatchers.IO) {
+            try {
+                guestProfiles = appDatabase?.guestProfileDao()?.getAll() ?: emptyList()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+        return guestProfiles.map { it.username }
     }
 }
---
+++ app/src/main/java/com/example/tournaMake/ui/screens/profile/ProfileListScreen.kt
@@ -0,0 +1,58 @@
+package com.example.tournaMake.ui.screens.profile
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.tournaMake.data.models.ThemeState
+import com.example.tournaMake.ui.screens.common.BasicScreenWithTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProfileListScreen(state: ThemeState, databaseList: List<String>) {
+    BasicScreenWithTheme(state = state) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TopAppBar(
+                title = { Text(text = "Profile Guest Screen") },
+                actions = {
+                    IconButton(onClick = { /* Do something when button is clicked */ }) {
+                        Icon(Icons.Filled.ArrowBack, contentDescription = null)
+                    }
+                }
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = { /* Do something when button is clicked */ }, modifier = Modifier.fillMaxWidth(0.9f).height(80.dp)) {
+                Text("My profile")
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            LazyColumn {
+                items(databaseList) { item ->
+                    Button(onClick = { /* Do something when button is clicked */ }, modifier = Modifier.fillMaxWidth(0.8f).height(60.dp)) {
+                        Text(item)
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
+            }
+        }
+    }
+}
