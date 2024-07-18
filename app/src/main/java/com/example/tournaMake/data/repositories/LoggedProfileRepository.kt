a125f59604b5eae8e134e0e4b27ec898fdb39388
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2024-07-18 23:10:58 +0200

    feat: added password management to LoggedProfileRepository

--- app/src/main/java/com/example/tournaMake/data/repositories/LoggedProfileRepository.kt
+++ app/src/main/java/com/example/tournaMake/data/repositories/LoggedProfileRepository.kt
@@ -6,9 +6,14 @@ import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import kotlinx.coroutines.flow.map
 
+/**
+ * This class will be used to store to shared preferences the username and the password
+ * of the last main profile user that logged in and opted to be remembered.
+ * */
 class LoggedProfileRepository(private val dataStore: DataStore<Preferences>) {
     companion object {
         private val LOGGED_EMAIL = stringPreferencesKey("logged_email")
+        private val USER_PASSWORD = stringPreferencesKey("password")
     }
     /*
     * If you forget to map the value using the companion object, than you obtain a strange
@@ -18,10 +23,15 @@ class LoggedProfileRepository(private val dataStore: DataStore<Preferences>) {
     * }
     * */
     val email = dataStore.data.map { data -> data[LOGGED_EMAIL] }
+    val password = dataStore.data.map { data -> data[USER_PASSWORD] }
 
     /**
      * This function should be called only if the user wants the app to remember their credentials.
      * */
     suspend fun setEmail(email: String) =
         dataStore.edit { it[LOGGED_EMAIL] = email }
+
+    suspend fun setPassword(password: String) {
+        dataStore.edit { it[USER_PASSWORD] = password }
+    }
 }
