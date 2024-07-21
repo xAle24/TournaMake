6a89629cd8484c843af7814a77f0638873b9453b
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2024-07-21 16:35:25 +0200

    feat: added bracket mapping from our data to library data

--- app/src/main/java/com/example/tournaMake/activities/TournamentActivity.kt
+++ app/src/main/java/com/example/tournaMake/activities/TournamentActivity.kt
@@ -2,9 +2,12 @@ package com.example.tournaMake.activities
 
 //import com.example.tournaMake.mylibrary.ui.SingleEliminationBracket
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.core.view.WindowCompat
+import androidx.lifecycle.Observer
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import com.example.tournaMake.data.models.ThemeViewModel
@@ -15,6 +18,7 @@ import com.example.tournaMake.mylibrary.displaymodels.BracketMatchDisplayModel
 import com.example.tournaMake.mylibrary.displaymodels.BracketRoundDisplayModel
 import com.example.tournaMake.mylibrary.displaymodels.BracketTeamDisplayModel
 import com.example.tournaMake.sampledata.AppDatabase
+import com.example.tournaMake.sampledata.TournamentMatchData
 import com.example.tournaMake.ui.screens.tournament.TournamentScreen
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -34,11 +38,22 @@ class TournamentActivity : ComponentActivity() {
             val tournamentIDViewModel = koinViewModel<TournamentIDViewModel>()
             val tournamentID = tournamentIDViewModel.tournamentID.collectAsStateWithLifecycle()
             val tournamentDataViewModel = koinViewModel<TournamentDataViewModel>()
-            fetchStuffForTournament(tournamentID.value, tournamentDataViewModel)
-            TournamentScreen(
-                state = state.value,
-                bracket = createBracket(tournamentDataViewModel)
+            val liveDataObserver = Observer<List<TournamentMatchData>> {
+                Log.d("DEV", "In TournamentActivity: Observer code called! List size = ${it.size}")
+                // When data arrives, create the bracket
+                //bracket = createBracket(tournamentDataViewModel)
+            }
+            tournamentDataViewModel.tournamentMatchesAndTeamsLiveData.observe(this, liveDataObserver)
+            val tournamentLiveData = tournamentDataViewModel.tournamentMatchesAndTeamsLiveData.observeAsState(
+                emptyList()
             )
+            fetchStuffForTournament(tournamentID.value, tournamentDataViewModel)
+            if (tournamentLiveData.value.isNotEmpty()) {
+                TournamentScreen(
+                    state = state.value,
+                    bracket = createBracket(tournamentDataViewModel)
+                )
+            }
             //SingleEliminationBracket(bracket = TestTournamentData.singleEliminationBracket)
         }
     }
@@ -49,23 +64,57 @@ class TournamentActivity : ComponentActivity() {
     ) {
         lifecycleScope.launch(Dispatchers.IO) {
             try {
-                val tournamentMatchesAndTeamsData = appDatabase.tournamentDao().getMatchesAndTeamsFromTournamentID(tournamentID)
+                val tournamentMatchesAndTeamsData =
+                    appDatabase.tournamentDao().getMatchesAndTeamsFromTournamentID(tournamentID)
                 tournamentDataViewModel.changeMatchesList(tournamentMatchesAndTeamsData)
             } catch (e: Exception) {
                 e.printStackTrace()
             }
         }
     }
-    private fun createBracket(tournament: TournamentDataViewModel): BracketDisplayModel {
 
-        val matches = BracketMatchDisplayModel()
-        val round: BracketRoundDisplayModel = BracketRoundDisplayModel("bao")
-        val bracket: BracketDisplayModel = BracketDisplayModel("one",)
-        tournament.tournamentMatchesAndTeamsLiveData.value?.forEach { team ->
-            val teamI = BracketTeamDisplayModel(team.name, false, "")
-            if (matches.)
-        }
-        return bracket
+    /**
+     * 1) bisogna aspettare che i live data arrivino
+     * 2) bisogna creare il bracket
+     * 3) bisogna creare la schermata
+     * */
+
+    /**
+     * Input: tournament data, che contiene team e match
+     * Bracket contiene più round
+     * I round (es. semifinali, finali) contengono team / 2 matches
+     * un match è giocato da 2 team
+     * */
+    private fun createBracket(tournament: TournamentDataViewModel): BracketDisplayModel {
+        val listOfTournamentData = tournament.tournamentMatchesAndTeamsLiveData.value ?: emptyList()
+        return if (listOfTournamentData.isNotEmpty()) BracketDisplayModel(
+            name = "MyBracket",
+            rounds = listOf(
+                BracketRoundDisplayModel(
+                    name = "Round 0",
+                    matches = generateSequence(0) { it + 2 }
+                        .take(listOfTournamentData.size / 2)
+                        .map { index ->
+                            Pair(
+                                BracketTeamDisplayModel(
+                                    name = listOfTournamentData[index].name,
+                                    isWinner = false,
+                                    score = "0"
+                                ),
+                                BracketTeamDisplayModel(
+                                    name = listOfTournamentData[index + 1].name,
+                                    isWinner = false,
+                                    score = "0"
+                                )
+                            )
+                        }
+                        .map {
+                            BracketMatchDisplayModel(it.first, it.second)
+                        }
+                        .toList()
+                )
+            )
+        ) else BracketDisplayModel("EmptyBracket", emptyList())
     }
 
     /**
--- app/src/main/java/com/example/tournaMake/ui/screens/tournament/TournamentScreen.kt
+++ app/src/main/java/com/example/tournaMake/ui/screens/tournament/TournamentScreen.kt
@@ -2,10 +2,23 @@ package com.example.tournaMake.ui.screens.tournament
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.tournaMake.data.models.ThemeState
 import com.example.tournaMake.mylibrary.displaymodels.BracketDisplayModel
 import com.example.tournaMake.mylibrary.ui.SingleEliminationBracket
@@ -31,13 +44,27 @@ fun TournamentView(
     bracket: BracketDisplayModel
 ) {
     ConfigureTransparentSystemBars()
-    SingleEliminationBracket(bracket = bracket)
-    /*Surface(
+
+    Surface(
         color = MaterialTheme.colorScheme.tertiary.copy(alpha = 0.8f), // the background behind the whole bracket system
         modifier = Modifier
             .systemBarsPadding(),
     ) {
-        val navController = rememberNavController()
+        SingleEliminationBracket(bracket = bracket)
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.BottomEnd
+        ) {
+            FloatingActionButton(
+                onClick = { /* Handle FAB click */ },
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(imageVector = Icons.Default.Edit, contentDescription = "Add")
+            }
+        }
+    }
+/*
+    val navController = rememberNavController()
 
         NavHost(navController = navController, startDestination = "home") {
             composable("home") {
