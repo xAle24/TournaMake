631d46b0860c707c583a51cbdb470f740975bd46
Alessio Lizza <alessio.lizza@studio.unibo.it>
2024-09-04 13:13:24 +0200

    fix: filters remember which filter is applied to match list

--- app/src/main/java/com/example/tournaMake/ui/screens/match/MatchListScreen.kt
+++ app/src/main/java/com/example/tournaMake/ui/screens/match/MatchListScreen.kt
@@ -1,7 +1,6 @@
 package com.example.tournaMake.ui.screens.match
 
 import Converters.fromTimestamp
-import android.util.Log
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -34,6 +33,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
 import androidx.compose.runtime.livedata.observeAsState
@@ -82,10 +82,10 @@ fun MatchListScreen(
     val colorConstants = getThemeColors(themeState = state)
     val searchbar2 = Searchbar(matchesList.value)
     var filteredEntries  = searchbar2.getFilteredEntries()
-    Log.d("CAZ", "$matchesList")
-    Log.d("DEV-MATCH-LIST", "Filtered entries: $filteredEntries")
     var showDialog by remember { mutableStateOf(false) }
     var selectedPredicate by remember { mutableStateOf<(MatchGameData) -> Boolean>({ true }) }
+    val options = listOf("All", "Favorites", "Completed")
+    val selectedOption = remember { mutableStateOf(options[0]) }
     BasicScreenWithAppBars(
         state = state,
         backFunction = { navController.navigate(NavigationRoute.MenuScreen.route) },
@@ -126,6 +126,8 @@ fun MatchListScreen(
         if (showDialog) {
             FilterDialog(
                 onDismiss = { showDialog = false },
+                options = options,
+                selectedOption = selectedOption,
                 onPredicateSelected = { predicate ->
                     selectedPredicate = predicate
                     searchbar2.filterEntries(predicate)
@@ -313,10 +315,11 @@ fun Content(
 @Composable
 fun FilterDialog(
     onDismiss: () -> Unit,
+    options: List<String>,
+    selectedOption: MutableState<String>,
     onPredicateSelected: (predicate: (MatchGameData) -> Boolean) -> Unit
 ) {
-    val options = listOf("All", "Favorites", "Completed")
-    var selectedOption by remember { mutableStateOf(options[0]) }
+
 
     AlertDialog(
         onDismissRequest = onDismiss,
@@ -328,8 +331,8 @@ fun FilterDialog(
                         verticalAlignment = Alignment.CenterVertically
                     ) {
                         RadioButton(
-                            selected = (selectedOption == option),
-                            onClick = { selectedOption = option }
+                            selected = (selectedOption.value == option),
+                            onClick = { selectedOption.value = option }
                         )
                         Text(option)
                     }
@@ -339,7 +342,7 @@ fun FilterDialog(
         confirmButton = {
             Button(
                 onClick = {
-                    val predicate: (MatchGameData) -> Boolean = when (selectedOption) {
+                    val predicate: (MatchGameData) -> Boolean = when (selectedOption.value) {
                         "Favorites" -> { match -> match.favorites == 1 }
                         "Completed" -> { match -> match.isOver == 1 }
                         else -> { _ -> true }
