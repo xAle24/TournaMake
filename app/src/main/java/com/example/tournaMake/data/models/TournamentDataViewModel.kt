ca17ecab3bd584b21263204a9169b4d929404c6f
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2024-09-05 12:30:52 +0200

    feat: added tournament name to TournamentDataViewModel

--- app/src/main/java/com/example/tournaMake/data/models/TournamentDataViewModel.kt
+++ app/src/main/java/com/example/tournaMake/data/models/TournamentDataViewModel.kt
@@ -8,8 +8,14 @@ import com.example.tournaMake.sampledata.TournamentMatchData
 class TournamentDataViewModel: ViewModel() {
     private val _tournamentData = MutableLiveData<List<TournamentMatchData>>()
     val tournamentMatchesAndTeamsLiveData : LiveData<List<TournamentMatchData>> = _tournamentData
+    private val _tournamentName = MutableLiveData<String>()
+    val tournamentName: LiveData<String> = _tournamentName
 
     fun changeMatchesList(list: List<TournamentMatchData>) {
         _tournamentData.postValue(list)
+    }
+
+    fun changeTournamentName(name: String) {
+        _tournamentName.postValue(name)
     }
 }
